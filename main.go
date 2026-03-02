7553777347d3016348ed12701dfef8a166e35a96
Самвел Сантурян <sant@MacBook-Air-Samvel.local>
2026-03-02 22:36:18 +0300

    Добавил структуры bin

--- main.go
+++ main.go
@@ -1,5 +1,51 @@
 package main
 
-func main(){
-	
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+type Bin struct {
+	id        string
+	private   bool
+	createdAt time.Time
+	name      string
+}
+type BinList = []Bin
+
+func getNewId(list BinList) string {
+	if len(list) >= 1 {
+		lastElementId := (list[len(list)-1].id)
+
+		lastElementIntId, _ := strconv.Atoi(lastElementId)
+
+		return strconv.Itoa(lastElementIntId + 1)
+
+	} else {
+		return "1"
+	}
+}
+func newBin(id string, name string, private bool) (*Bin, error) {
+	if id == "" || name == ""{
+		return nil, errors.New("Empty id or name")
+	}
+	currentBin := Bin{
+		id:      id,
+		name:    name,
+		private: private,
+		createdAt: time.Now(),
+	}
+	return &currentBin,nil
+
+}
+func main() {
+	list := BinList{}
+	newId := getNewId(list)
+	bin, err := newBin(newId, "sam", true)
+	if err != nil {
+		return
+	}
+	fmt.Println(bin)
 }
