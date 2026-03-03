24bb00734ca89f188b3648c53e50ed015eb7d1a8
Самвел Сантурян <sant@Mac.ivstar.local>
2026-03-03 13:37:39 +0300

    Выделил пакеты

---
+++ bins/bins.go
@@ -0,0 +1,41 @@
+package bins
+
+import (
+	"errors"
+	"strconv"
+	"time"
+)
+
+type Bin struct {
+	id        string
+	private   bool
+	createdAt time.Time
+	name      string
+}
+type BinList = []Bin
+
+func GetNewId(list BinList) string {
+	if len(list) >= 1 {
+		lastElementId := (list[len(list)-1].id)
+
+		lastElementIntId, _ := strconv.Atoi(lastElementId)
+
+		return strconv.Itoa(lastElementIntId + 1)
+
+	} else {
+		return "1"
+	}
+}
+func NewBin(id string, name string, private bool) (*Bin, error) {
+	if id == "" || name == ""{
+		return nil, errors.New("Empty id or name")
+	}
+	currentBin := Bin{
+		id:      id,
+		name:    name,
+		private: private,
+		createdAt: time.Now(),
+	}
+	return &currentBin,nil
+
+}
--- main.go
+++ main.go
@@ -1,49 +1,14 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"strconv"
-	"time"
+	"demo/struct/bins"
 )
 
-type Bin struct {
-	id        string
-	private   bool
-	createdAt time.Time
-	name      string
-}
-type BinList = []Bin
-
-func getNewId(list BinList) string {
-	if len(list) >= 1 {
-		lastElementId := (list[len(list)-1].id)
-
-		lastElementIntId, _ := strconv.Atoi(lastElementId)
-
-		return strconv.Itoa(lastElementIntId + 1)
-
-	} else {
-		return "1"
-	}
-}
-func newBin(id string, name string, private bool) (*Bin, error) {
-	if id == "" || name == ""{
-		return nil, errors.New("Empty id or name")
-	}
-	currentBin := Bin{
-		id:      id,
-		name:    name,
-		private: private,
-		createdAt: time.Now(),
-	}
-	return &currentBin,nil
-
-}
 func main() {
-	list := BinList{}
-	newId := getNewId(list)
-	bin, err := newBin(newId, "sam", true)
+	list := bins.BinList{}
+	newId := bins.GetNewId(list)
+	bin, err := bins.NewBin(newId, "sam", true)
 	if err != nil {
 		return
 	}
